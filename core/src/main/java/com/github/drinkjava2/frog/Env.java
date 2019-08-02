9d7248b0026f4d5e56d1b7ec7c460b258c8b0c20
Yong Zhu <yong9981@gmail.com>
2019-08-02 07:53:02 -0600

    Debuging pickFromGroup

--- core/src/main/java/com/github/drinkjava2/frog/Env.java
+++ core/src/main/java/com/github/drinkjava2/frog/Env.java
@@ -5,7 +5,10 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 import javax.swing.JPanel;
@@ -24,10 +27,18 @@ import com.github.drinkjava2.frog.util.RandomUtils;
 @SuppressWarnings("serial")
 public class Env extends JPanel {
 	/** Speed of test */
-	public static int SHOW_SPEED = 2; // 测试速度，-1000~1000,可调, 数值越小，速度越慢
+	public static final int SHOW_SPEED = 800; // 测试速度，-1000~1000,可调, 数值越小，速度越慢
 
 	/** Delete eggs at beginning of each run */
-	public static final boolean DELETE_EGGS = false;// 每次运行是否先删除保存的蛋
+	public static final boolean DELETE_EGGS = true;// 每次运行是否先删除保存的蛋
+
+	public static final int EGG_QTY = 1; // 每轮下n个蛋，可调，只有最优秀的前n个青蛙们才允许下蛋
+
+	public static final int FROG_PER_EGG = 1; // 每个蛋可以孵出几个青蛙
+
+	public static final int GROUP_SIZE = 12;// 分组测试，每个组里面有多少轮, 利用分组可以在慢的电脑上每次只跑少量的样本，用时间换空间
+
+	public static final int PICK_PER_GROUP = 4;// 一组被测试完后，只有总找食最多的若干组被选中参与下一组测试
 
 	/** Debug mode will print more debug info */
 	public static final boolean DEBUG_MODE = false; // Debug 模式下会打印出更多的调试信息
@@ -52,10 +63,6 @@ public class Env extends JPanel {
 
 	public static final int FOOD_QTY = 1500; // 食物数量, 可调
 
-	public static final int EGG_QTY = 3; // 每轮下n个蛋，可调，只有最优秀的前n个青蛙们才允许下蛋
-
-	public static final int FROG_PER_EGG = 2; // 每个蛋可以孵出几个青蛙
-
 	private static final Random r = new Random(); // 随机数发生器
 
 	public static boolean pause = false; // 暂停按钮按下将暂停测试
@@ -66,9 +73,9 @@ public class Env extends JPanel {
 
 	private static final int TRAP_HEIGHT = 10; // 陷阱宽, 0~200
 
-	public List<Frog> frogs = new ArrayList<>();
+	public static List<Frog> frogs = new ArrayList<>();
 
-	public List<Egg> eggs;
+	public static Map<Float, List<Egg>> eggsMap;
 
 	static {
 		System.out.println("唵缚悉波罗摩尼莎诃!"); // 往生咒
@@ -115,7 +122,7 @@ public class Env extends JPanel {
 		return false;
 	}
 
-	private void rebuildFrogAndFood() {
+	private void rebuildFrogAndFood(List<Egg> eggs) {
 		frogs.clear();
 		for (int i = 0; i < ENV_WIDTH; i++) {// 清除食物
 			for (int j = 0; j < ENV_HEIGHT; j++) {
@@ -125,7 +132,7 @@ public class Env extends JPanel {
 		Random rand = new Random();
 		for (int i = 0; i < eggs.size(); i++) {// 创建青蛙，每个蛋生成4个蛙，并随机取一个别的蛋作为精子
 			int loop = FROG_PER_EGG;
-			if (eggs.size() > 20) { //如果数量多，进行一些优化，让排名靠前的Egg多孵出青蛙
+			if (eggs.size() > 20) { // 如果数量多，进行一些优化，让排名靠前的Egg多孵出青蛙
 				if (i < FROG_PER_EGG)// 0,1,2,3
 					loop = FROG_PER_EGG + 1;
 				if (i >= (eggs.size() - FROG_PER_EGG))
@@ -163,13 +170,23 @@ public class Env extends JPanel {
 		format100.setMaximumFractionDigits(2);
 	}
 
-	private String foodFoundPercent() {// 计算找食效率
+	private static int foodFoundAmount() {// 统计找食数等
 		int leftfood = 0;
 		for (int x = 0; x < ENV_WIDTH; x++)
 			for (int y = 0; y < ENV_HEIGHT; y++)
 				if (foods[x][y])
 					leftfood++;
-		return format100.format((FOOD_QTY - leftfood) * 1.00 / FOOD_QTY);
+		return FOOD_QTY - leftfood;
+	}
+
+	private String foodFoundCountText() {// 统计找食数等
+		int foodFound = foodFoundAmount();
+		int maxFound = 0;
+		for (Frog f : frogs)
+			if (f.ateFood > maxFound)
+				maxFound = f.ateFood;
+		return new StringBuilder("找食率:").append(format100.format(foodFound * 1.00 / FOOD_QTY)).append(", 平均: ")
+				.append(foodFound * 1.0f / (EGG_QTY * FROG_PER_EGG)).append("，最多:").append(maxFound).toString();
 	}
 
 	private static void sleep(long millis) {
@@ -181,72 +198,111 @@ public class Env extends JPanel {
 		}
 	}
 
+	@SuppressWarnings("unused")
 	public void run() throws InterruptedException {
-		EggTool.loadEggs(this); // 从磁盘加载egg，或新建一批egg
+		EggTool.loadEggs(); // 从磁盘加载egg，或新建一批egg
 		int round = 1;
 		Image buffImg = createImage(this.getWidth(), this.getHeight());
 		Graphics g = buffImg.getGraphics();
-		long t1, t2;
+		long t1, t2;// 计时用
 		do {
-			if (pause) {
-				sleep(300);
-				continue;
-			}
-			t1 = System.currentTimeMillis();
-			rebuildFrogAndFood();
-			boolean allDead = false;
-			Frog firstFrog = frogs.get(0);
-			for (int i = 0; i < STEPS_PER_ROUND; i++) {
-				if (allDead) {
-					System.out.println("All dead at round:" + i);
-					break; // 青蛙全死光了就直接跳到下一轮,以节省时间
-				}
-				allDead = true;
-				for (Frog frog : frogs)
-					if (frog.active(this))
-						allDead = false;
+			Map<Float, List<Egg>> resultEggsMap = new HashMap<Float, List<Egg>>();
+			for (List<Egg> eggs : eggsMap.values()) {
+				if (pause)
+					do {
+						sleep(300);
+					} while (pause);
+				t1 = System.currentTimeMillis();
+				rebuildFrogAndFood(eggs);
+				boolean allDead = false;
+				Frog firstFrog = frogs.get(0);
+				for (int i = 0; i < STEPS_PER_ROUND; i++) {
+					if (allDead) {
+						System.out.println("All dead at round:" + i);
+						break; // 青蛙全死光了就直接跳到下一轮,以节省时间
+					}
+					allDead = true;
+					for (Frog frog : frogs)
+						if (frog.active(this))
+							allDead = false;
 
-				for (Frog frog : frogs)
-					if (frog.alive && RandomUtils.percent(0.2f)) {// 有很小的机率在青蛙活着时就创建新的器官
-						RandomConnectGroup newConGrp = new RandomConnectGroup();
-						newConGrp.initFrog(frog);
-						frog.organs.add(newConGrp);
+					for (Frog frog : frogs)
+						if (frog.alive && RandomUtils.percent(0.2f)) {// 有很小的机率在青蛙活着时就创建新的器官
+							RandomConnectGroup newConGrp = new RandomConnectGroup();
+							newConGrp.initFrog(frog);
+							frog.organs.add(newConGrp);
+						}
+
+					if (SHOW_SPEED > 0 && i % SHOW_SPEED != 0) // 画青蛙会拖慢速度
+						continue;
+
+					if (SHOW_SPEED < 0) // 如果speed小于0，人为加入延迟
+						sleep(-SHOW_SPEED);
+
+					// 开始画青蛙
+					g.setColor(Color.white);
+					g.fillRect(0, 0, this.getWidth(), this.getHeight());
+					g.setColor(Color.BLACK);
+					for (Frog frog : frogs)
+						frog.show(g);
+
+					if (firstFrog.alive) { // 开始显示第一个Frog的动态脑图
+						if (Application.SHOW_FIRST_FROG_BRAIN) {
+							g.setColor(Color.red);
+							g.drawArc(firstFrog.x - 15, firstFrog.y - 15, 30, 30, 0, 360);
+							g.setColor(Color.BLACK);
+						}
+						if (DRAW_BRAIN_AFTER_STEPS > 0 && i % DRAW_BRAIN_AFTER_STEPS == 0)
+							Application.brainPic.drawBrainPicture(firstFrog);
 					}
 
-				if (SHOW_SPEED > 0 && i % SHOW_SPEED != 0) // 画青蛙会拖慢速度
-					continue;
+					drawTrap(g);
+					drawFood(g);
+					Graphics g2 = this.getGraphics();
+					g2.drawImage(buffImg, 0, 0, this);
 
-				if (SHOW_SPEED < 0) // 如果speed小于0，人为加入延迟
-					sleep(-SHOW_SPEED);
-
-				// 开始画青蛙
-				g.setColor(Color.white);
-				g.fillRect(0, 0, this.getWidth(), this.getHeight());
-				g.setColor(Color.BLACK);
-				for (Frog frog : frogs)
-					frog.show(g);
-
-				if (firstFrog.alive) { // 开始显示第一个Frog的动态脑图
-					if (Application.SHOW_FIRST_FROG_BRAIN) {
-						g.setColor(Color.red);
-						g.drawArc(firstFrog.x - 15, firstFrog.y - 15, 30, 30, 0, 360);
-						g.setColor(Color.BLACK);
-					}
-					if (DRAW_BRAIN_AFTER_STEPS > 0 && i % DRAW_BRAIN_AFTER_STEPS == 0)
-						Application.brainPic.drawBrainPicture(firstFrog);
 				}
-
-				drawTrap(g);
-				drawFood(g);
-				Graphics g2 = this.getGraphics();
-				g2.drawImage(buffImg, 0, 0, this);
-
+				Application.brainPic.drawBrainPicture(firstFrog);
+				EggTool.layEggs(foodFoundAmount(), resultEggsMap);
+				t2 = System.currentTimeMillis();
+				Application.mainFrame.setTitle(new StringBuilder("轮数: ").append(round++).append(", ")
+						.append(foodFoundCountText()).append(", 用时: ").append(t2 - t1).append("ms").toString());
 			}
-			Application.brainPic.drawBrainPicture(firstFrog);
-			EggTool.layEggs(this);
-			t2 = System.currentTimeMillis();
-			Application.mainFrame.setTitle("Frog test round: " + round++ + ", 找食效率:" + foodFoundPercent()
-					+ ", time used: " + (t2 - t1) + " ms");
+			pickFromResultMap(resultEggsMap);
 		} while (true);
+	}
+
+	private void pickFromResultMap(Map<Float, List<Egg>> resultEggsMap) {// 从测试组里PICK出找食最多的几组，其余的组被淘汰
+		eggsMap.clear();
+		Object[] key = resultEggsMap.keySet().toArray();
+		Arrays.sort(key);
+		for (Object object : key) {
+			System.out.print(object+",");
+		}
+		System.out.println();
+		for (int i = 0; i <PICK_PER_GROUP  ; i++) {
+			System.out.println("i="+i);
+			System.out.println(" key.length="+ key.length);
+			System.out.println("PICK_PER_GROUP="+ PICK_PER_GROUP);
+			System.out.println("GROUP_SIZE="+ GROUP_SIZE);
+			System.out.println("GROUP_SIZE / PICK_PER_GROUP="+ (GROUP_SIZE / PICK_PER_GROUP));
+			for (int j = 0; j < (GROUP_SIZE / PICK_PER_GROUP); j++) { // group 10 / pick 2 =5
+				eggsMap.put(Float.parseFloat("0." + j), resultEggsMap.get(key[resultEggsMap.size()-1-i   ]));
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Map<Float, List<Egg>> r = new HashMap<>();
+		r.put(3.1f, null);
+		r.put(12.1f, null);
+		r.put(0.1f, null);
+		r.put(1.1f, null);
+		r.put(10.1f, null);
+		Object[] key = r.keySet().toArray();
+		Arrays.sort(key);
+		for (Object object : key) {
+			System.out.println(object);
+		}
 	}
 }
--- core/src/main/java/com/github/drinkjava2/frog/egg/EggTool.java
+++ core/src/main/java/com/github/drinkjava2/frog/egg/EggTool.java
@@ -18,7 +18,9 @@ import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.github.drinkjava2.frog.Application;
 import com.github.drinkjava2.frog.Env;
@@ -42,11 +44,11 @@ public class EggTool {
 	 * 利用Java串行机制存盘。 能量多(也就是吃的更多，更fat)的Frog下蛋并存盘, 以进行下一伦测试，能量少的Frog被淘汰，没有下蛋的资格。
 	 * 用能量的多少来简化生存竟争模拟
 	 */
-	public static void layEggs(Env env) {
-		sortFrogsOrderByEnergyDesc(env);
+	public static void layEggs(int foundFound, Map<Float, List<Egg>> resultEggsMap) {
+		sortFrogsOrderByEnergyDesc();
 
-		Frog first = env.frogs.get(0);
-		Frog last = env.frogs.get(env.frogs.size() - 1);
+		Frog first = Env.frogs.get(0);
+		Frog last = Env.frogs.get(Env.frogs.size() - 1);
 
 		if (Env.DEBUG_MODE)
 			for (int i = 0; i < first.organs.size(); i++) {
@@ -55,30 +57,30 @@ public class EggTool {
 						+ org.organActiveEnergy + ", outputEnergy=" + org.organOutputEnergy);
 			}
 
-		System.out.println("1st frog has " + first.organs.size() + " organs, energy=" + first.energy + ", seeDist="
+		System.out.print("\r1st frog has " + first.organs.size() + " organs, energy=" + first.energy + ", seeDist="
 				+ ((Eye) first.organs.get(6)).seeDistance + ", chance=" + ((Chance) first.organs.get(10)).percent);
-		System.out.println("Last frog has " + last.organs.size() + " organs, energy=" + last.energy);
+		System.out.println(", Last frog has " + last.organs.size() + " organs, energy=" + last.energy);
 
 		try {
 			List<Egg> newEggs = new ArrayList<>();
 			for (int i = 0; i < Env.EGG_QTY; i++)
-				newEggs.add(new Egg(env.frogs.get(i)));
-
-			FileOutputStream fo = new FileOutputStream(Application.CLASSPATH + "eggs.ser");
-			ObjectOutputStream so = new ObjectOutputStream(fo);
-			so.writeObject(newEggs);
-			so.close();
-
-			env.eggs = newEggs;
-			System.out
-					.println("Saved " + env.eggs.size() + " eggs to file '" + Application.CLASSPATH + "eggs.ser" + "'");
+				newEggs.add(new Egg(Env.frogs.get(i)));
+			resultEggsMap.put(Float.parseFloat(foundFound + "." + resultEggsMap.size()), newEggs);
+			if (resultEggsMap.size() == Env.GROUP_SIZE) {
+				FileOutputStream fo = new FileOutputStream(Application.CLASSPATH + "eggs.ser");
+				ObjectOutputStream so = new ObjectOutputStream(fo);
+				so.writeObject(resultEggsMap);
+				so.close();
+				System.out.println("Saved 1 group eggs to file '" + Application.CLASSPATH + "eggs.ser'");
+			} else
+				System.out.println("Stored " + newEggs.size() + " eggs to group in memory.");
 		} catch (IOException e) {
 			System.out.println(e);
 		}
 	}
 
-	private static void sortFrogsOrderByEnergyDesc(Env env) {// 按能量多少给青蛙排序
-		Collections.sort(env.frogs, new Comparator<Frog>() {
+	private static void sortFrogsOrderByEnergyDesc() {// 按能量多少给青蛙排序
+		Collections.sort(Env.frogs, new Comparator<Frog>() {
 			public int compare(Frog a, Frog b) {
 				if (a.energy > b.energy)
 					return -1;
@@ -117,25 +119,31 @@ public class EggTool {
 	 * 从磁盘读入一批Egg
 	 */
 	@SuppressWarnings("unchecked")
-	public static void loadEggs(Env env) {
+	public static void loadEggs() {
 		boolean errorfound = false;
 		try {
 			FileInputStream eggsFile = new FileInputStream(Application.CLASSPATH + "eggs.ser");
 			ObjectInputStream eggsInputStream = new ObjectInputStream(eggsFile);
-			env.eggs = (List<Egg>) eggsInputStream.readObject();
-			System.out.println(
-					"Loaded " + env.eggs.size() + " eggs from file '" + Application.CLASSPATH + "eggs.ser" + "'.");
+			Env.eggsMap = (Map<Float, List<Egg>>) eggsInputStream.readObject();
+			System.out.println("Loaded " + Env.eggsMap.size() + " egg groups from file '" + Application.CLASSPATH
+					+ "eggs.ser" + "'.");
 			eggsInputStream.close();
 		} catch (Exception e) {
 			errorfound = true;
 		}
 		if (errorfound) {
-			System.out.println("Fail to load egg file at path '" + Application.CLASSPATH + "', created " + Env.EGG_QTY
-					+ " new eggs to do test.");
-			env.eggs = new ArrayList<Egg>();
-			for (int i = 0; i < Env.EGG_QTY; i++)
-				env.eggs.add(new Egg());
-		} 
+			System.out.println("Fail to load egg file at path '" + Application.CLASSPATH + "', created "
+					+ Env.GROUP_SIZE + " egg groups to do test.");
+			Env.eggsMap = new HashMap<Float, List<Egg>>();
+		}
+		if (Env.eggsMap.size() < Env.GROUP_SIZE)
+			for (int i = Env.eggsMap.size(); i < Env.GROUP_SIZE; i++) {
+				List<Egg> eggLst = new ArrayList<Egg>();
+				for (int j = 0; j < Env.EGG_QTY; j++)
+					eggLst.add(new Egg());
+				Env.eggsMap.put(Float.parseFloat("0." + i), eggLst);// 0 means current egg' father found 0 food
+			}
+
 	}
 
 }
