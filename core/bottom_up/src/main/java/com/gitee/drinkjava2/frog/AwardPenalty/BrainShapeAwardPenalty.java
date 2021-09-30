f22ab37e8982933e847426987a8abae73b4938ca
yong <yong9981@gmail.com>
2021-09-30 08:21:09 -0600

    Work on braishape

--- core/bottom_up/src/main/java/com/gitee/drinkjava2/frog/AwardPenalty/BrainShapeAwardPenalty.java
+++ core/bottom_up/src/main/java/com/gitee/drinkjava2/frog/AwardPenalty/BrainShapeAwardPenalty.java
@@ -9,8 +9,15 @@ import com.gitee.drinkjava2.frog.Env;
  *  这个类的awardPenalty方法在动物的初始化后被调用，根据脑细胞的三维结构形状来对动物进行奖罚，即加减它的能量值，这是一个临时类，只是用来检验细胞三维成形功能，以后可能改名或删除
  */
 public class BrainShapeAwardPenalty {
-    private static boolean[][][] brainShape = new boolean[Env.BRAIN_XSIZE][Env.BRAIN_YSIZE][Env.BRAIN_ZSIZE];
+    private static byte[][][] brainShape = new byte[Env.BRAIN_XSIZE][Env.BRAIN_YSIZE][Env.BRAIN_ZSIZE];
 
+    private static byte[][] pic= new byte[Env.BRAIN_YSIZE][Env.BRAIN_ZSIZE];
+    
+    static {
+        
+        brainShape[1]=pic;
+    }
+    
     public static void execute(Animal animal) {//检查animal的脑细胞是否位于brainShape的范围内
         //TODO 检查animal的脑细胞是否位于brainShape的范围内
     }
