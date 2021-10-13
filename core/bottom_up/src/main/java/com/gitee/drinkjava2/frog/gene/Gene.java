ce8dccda36ee29e072c2f23e17e3017d38d2ad90
Yong <yong9981@gmail.com>
2021-10-13 14:45:10 -0600

    Clean up, delete unused Gene methods

--- core/bottom_up/src/main/java/com/gitee/drinkjava2/frog/gene/Gene.java
+++ core/bottom_up/src/main/java/com/gitee/drinkjava2/frog/gene/Gene.java
@@ -37,10 +37,11 @@ public class Gene {// NOSONAR
     private static int index = 9; //关键字是一个两位数字字符，从10开始依次往下排。关键字没有可读性，调试时要用printGene方法将关键字转为可读的语句
     public static String[] TEXT = new String[20]; //这里存放关键字的文字解释，供打印输出用
 
-    public static final int GOTO = nextKeyword("GOTO"); //GOTO关键字=10
-    public static final int END = nextKeyword("END"); //结束执行=11
-    public static final int SPLIT = nextKeyword("SPLIT"); //执行细胞分裂， 分裂方向由第二部分的数值决定，一个细胞有可能同时在多个方向分裂出多个细胞，有6个或27个方向等
-    public static final int SPLIT_LIMIT = nextKeyword("SPLIT_LIMIT"); //细胞分裂寿命,  0表示可以无限分裂
+    //下面这些基因关键字是失败的尝试，从2021-10-13这个提交开始不再使用
+    //    public static final int GOTO = nextKeyword("GOTO"); //GOTO关键字=10
+    //    public static final int END = nextKeyword("END"); //结束执行=11
+    //    public static final int SPLIT = nextKeyword("SPLIT"); //执行细胞分裂， 分裂方向由第二部分的数值决定，一个细胞有可能同时在多个方向分裂出多个细胞，有6个或27个方向等
+    //    public static final int SPLIT_LIMIT = nextKeyword("SPLIT_LIMIT"); //细胞分裂寿命,  0表示可以无限分裂
     public static final int NEW_CELL = nextKeyword("NEW_CELL"); //在新位置新创建一个细胞，而不是由其它细胞分裂出来
 
     public static final int FIRST_KEYWORD = NEW_CELL;
@@ -73,64 +74,8 @@ public class Gene {// NOSONAR
             int code = toCode(gene);
             int param = toParam(gene);
             String paramStr = "" + param;
-            if (code == SPLIT) {
-                sb.setLength(0);
-                if ((param & 1) > 0)
-                    sb.append("U");//上
-                else if ((param & 0b10) > 0)
-                    sb.append("D");//下
-                else if ((param & 0b100) > 0)
-                    sb.append("L");//左
-                else if ((param & 0b1000) > 0)
-                    sb.append("R");//右
-                else if ((param & 0b10000) > 0)
-                    sb.append("F");//前
-                else if ((param & 0b100000) > 0)
-                    sb.append("B");//后
-                paramStr = sb.toString();
-            }
             System.out.println(i++ + " " + TEXT[code] + " " + paramStr);
         }
-    }
-
-    //execute gene language for one cell only 
-    public static void run(Animal animal, Cell cell) { //对于给定的细胞，由基因、这个细胞所处的行号、细胞的分裂寿命、细胞已分裂的次数、以及细胞所处的身体坐标、以及细胞周围是否有细胞包围来决定它的下一步分裂行为
-        if (cell.geneIndex < 0 || cell.geneIndex >= animal.gene.size() || cell.splitCount >= cell.splitLimit)
-            return;
-        long gene = animal.gene.get(cell.geneIndex);
-        int code = toCode(gene);
-        int param = toParam(gene);
-
-        if (code == END) {//如果是END, 结束分裂，参数就不需要解读了
-            cell.geneIndex = -1;//改为-1,以后直接跳过这个细胞，不再执行上面的Integer.parseInt
-            return;
-        }
-
-        cell.geneIndex++;
-
-        if (code == GOTO) {
-            if (param < 0 || param >= animal.gene.size()) {//行号太大、太小都不行
-                animal.kill();
-                return;
-            }
-            cell.geneIndex = param;
-            return;
-        } else if (code == SPLIT_LIMIT) {//重定义细胞寿命
-            cell.splitLimit = param;
-            return;
-        } else if (code == SPLIT) { //执行细胞分裂 
-            cell.splitCount++;
-            if (param < 0 || param > 63) //如果是分裂的话，param应该随机生成落在0~63之内，每个二进制的一个位代表1个分裂方向，共有上下左右前后6个方向
-                return;
-            cell.split(animal, param);//cell在参数代表的方向进行分裂克隆，可以同时在多个方向克隆出多个细胞
-        } else if (code == NEW_CELL) { //新建一个细胞，从头开始分裂，它的splitCount=0;
-            int x = param / 1000000;
-            int y = (param - x * 1000000) / 1000;
-            int z = param % 1000;
-            Cell c = new Cell(animal, x, y, z, cell.geneIndex, 0, 10);
-            c.color = Color.RED;
-        }
-        return;
     }
 
     public static void run(Animal animal) { //对于给定的细胞，由基因、这个细胞所处的行号、细胞的分裂寿命、细胞已分裂的次数、以及细胞所处的身体坐标、以及细胞周围是否有细胞包围来决定它的下一步分裂行为
@@ -156,15 +101,7 @@ public class Gene {// NOSONAR
 
     public static int randomGeneParam(Animal animal, long code) {//根据基因code生成一个随机合理参数
         int param = 0;
-        if (code == GOTO) {
-            param = RandomUtils.nextInt(animal.gene.size());
-        } else if (code == SPLIT_LIMIT) {//细胞寿命
-            param = RandomUtils.nextInt(10) + 1; //注： 细胞寿命这个魔数以后要写在基因里
-        } else if (code == SPLIT) { //细胞分裂，暂定只有6个方向，用整数的6个位表示
-            param = RandomUtils.nextInt(2);
-            for (int j = 0; j < 5; j++)
-                param = param * 2 + RandomUtils.nextInt(2);
-        } else if (code == NEW_CELL) { //新的细胞坐标位置参数，用一个整数表示, 值为 X*1000000 + Y*1000 + Z
+        if (code == NEW_CELL) { //新的细胞坐标位置参数，用一个整数表示, 值为 X*1000000 + Y*1000 + Z
             if (animal.cells.isEmpty()) { //如果animal没有细胞，位置取脑的中间
                 return Env.BRAIN_XSIZE / 2 * 1000000 + Env.BRAIN_YSIZE / 2 * 1000 + Env.BRAIN_ZSIZE / 2;
             } else { //如果脑细胞已有了，随机取一个脑细胞，返回它的一个相邻坐标位置
@@ -172,9 +109,9 @@ public class Gene {// NOSONAR
                 int x = c.x;
                 int y = c.y;
                 int z = c.z;
-                x+=RandomUtils.nextInt(5)-2;
-                y+=RandomUtils.nextInt(5)-2;
-                z+=RandomUtils.nextInt(5)-2;
+                x += RandomUtils.nextInt(5) - 2;
+                y += RandomUtils.nextInt(5) - 2;
+                z += RandomUtils.nextInt(5) - 2;
                 return x * 1000000 + y * 1000 + z;
             }
         }
