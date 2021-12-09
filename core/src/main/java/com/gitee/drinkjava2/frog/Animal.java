161089c049c961f07dd4172a0852b3b8488369c7
yong <yong9981@gmail.com>
2021-12-09 08:13:24 -0700

    tiny change

--- core/src/main/java/com/gitee/drinkjava2/frog/Animal.java
+++ core/src/main/java/com/gitee/drinkjava2/frog/Animal.java
@@ -21,6 +21,7 @@ import java.util.ArrayList;
 import javax.imageio.ImageIO;
 
 import com.gitee.drinkjava2.frog.egg.Egg;
+import com.gitee.drinkjava2.frog.judge.BrainShapeJudge;
 import com.gitee.drinkjava2.frog.objects.Material;
 import com.gitee.drinkjava2.frog.util.RandomUtils;
 import com.gitee.drinkjava2.frog.util.Tree8Util;
@@ -117,6 +118,7 @@ public abstract class Animal {// 这个程序大量用到public变量而不是ge
     public void initAnimal() { // 初始化animal,生成脑细胞是在这一步，这个方法是在当前屏animal生成之后调用，比方说有一千个青蛙分为500屏测试，每屏只生成2个青蛙的脑细胞，可以节约内存
         geneMutation(); //有小概率基因突变
         createCellsFromGene(); //运行基因语言，生成脑细胞
+        BrainShapeJudge.judge(this);
     }
 
     public boolean active() {// 这个active方法在每一步循环都会被调用，是脑思考的最小帧
@@ -164,8 +166,12 @@ public abstract class Animal {// 这个程序大量用到public变量而不是ge
                     }
                 }
             }
-
         }
+        
+        if (RandomUtils.percent(10)) {
+            
+        }
+        
     }
 
     private void createCellsFromGene() {//根据基因生成细胞参数  
